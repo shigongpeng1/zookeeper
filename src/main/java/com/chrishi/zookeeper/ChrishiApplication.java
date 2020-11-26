af5fb7798b6726cd83b3b6909b32912ef9983e71
shigongpeng <503484839@qq.com>
2020-11-26 22:31:36 +0800

    init

---
+++ src/main/java/com/chrishi/zookeeper/ChrishiApplication.java
@@ -0,0 +1,13 @@
+package com.chrishi.zookeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChrishiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChrishiApplication.class, args);
+    }
+
+}
